383a26f42bbc17c40bba87e8d7a3ef1b1d23b635
mustafaaadil448 <mdmustafa8210@gmail.com>
2025-08-01 07:32:06 +0530

    Update Successfull

--- server/index.js
+++ server/index.js
@@ -1,17 +1,18 @@
-import dotenv from "dotenv";
-dotenv.config();
-import express from "express";
 import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
 import connectDB from "./config/db.js";
 import userRouter from "./route/user.route.js";
+dotenv.config();
 
 const app = express();
 
-// middlewares
+// middlewares.
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
+// Set security headers
 app.get("/", (req, res) => {
     console.log("API is running...");
     res.send("API is running...");
